e3b89b656aa6d705b7d217caaa82623f05f2d99a
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-12-04 19:58:12 +0000

    fix - Cloudflware linter unused vars

--- frontend/src/Pages/ShopCategory/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory/ShopCategory.jsx
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { ShopContext } from "../../Context/ShopContext";
 import { BannerCategory } from "../../Components/BannerCategory/BannerCategory";
 import { ItemList } from "../../Components/ItemList/ItemList";
