324ce8461933220290f446c3342aaa37db08a814
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-12-04 17:01:28 +0000

    add more items to NewCollections

--- frontend/src/Assets/products/product_data.js
+++ frontend/src/Assets/products/product_data.js
@@ -381,7 +381,7 @@ let products = [
     images: [p19_img],
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    tags: ["modern", "latest"],
+    tags: ["new", "modern", "latest"],
     category: "men",
     rating: 3.5,
     stock: {
@@ -435,7 +435,7 @@ let products = [
     images: [p22_img],
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    tags: ["modern", "latest"],
+    tags: ["new", "modern", "latest"],
     category: "men",
     rating: 3.5,
     stock: {
